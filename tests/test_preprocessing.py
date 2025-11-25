19dbcae0be8a6cb67ca47439415925e9e90826e7
tsanhith <sanhith6@gmail.com>
2025-11-25 09:35:30 +0530

    test(preproc): add build_pipeline unit test

---
+++ tests/test_preprocessing.py
@@ -0,0 +1,8 @@
+﻿from src.preprocessing import build_pipeline
+import pandas as pd
+
+def test_build_pipeline_runs():
+    df = pd.DataFrame({'SalePrice':[100,200], 'A':[1,2], 'B':[3,4]})
+    X, y, p = build_pipeline(df)
+    assert X.shape[0] == 2
+    assert 'imputer' in p and 'scaler' in p
